4b62edfe4ca1ed2d60bf6b566a2a77d228884472
whybruno.dev@gmail.com <whybruno.dev@gmail.com>
2023-11-26 15:29:39 +0000

    Fix #menu-icon and #hidden-menu to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-// menu-icon open and close function
+// menu-icon transition effect
 
-function myFunction(x) {
-  x.classList.toggle("change");
-}var menuButton = document.getElementById("menu-icon");
+// function myFunction(x) {
+//   x.classList.toggle("change");
+// }
 
-// hidden-menu display function
-
+var menuButton = document.getElementById("menu-icon");
+var links = document.getElementsByClassName('nav-link');
 var menu = document.getElementById("hidden-menu");
+
+// hidden-menu display change
 
 menuButton.addEventListener("click", function() {
   if (menu.style.display === "block") {
@@ -15,4 +17,25 @@ menuButton.addEventListener("click", function() {
     menu.style.display = "block";
   }
 });
+
+// hidden-menu after any nav-link is clicked
+
+for (const link of links) {
+  link.addEventListener('click', function onClick() {
+    menu.style.display = "none";
+  });
+}
+
+// // hidden-menu after single nav-link is clicked
+
+// menuItem[0].addEventListener('click', function onClick() {
+//   if (menu.style.display === "block") {
+//     menu.style.display = "none";
+//   }
+// });
+
+
+
+
+
 
